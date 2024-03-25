402842467fb428704f56a62591709560e4b17df9
Vimal Kaiprath <vkaiprath@ibizsoftinc.com>
2024-03-25 22:12:38 +0530

    Abstract keyword

--- AbstractKeyword.java
+++ AbstractKeyword.java
@@ -1,3 +1,19 @@
 public class AbstractKeyword {
+    public static void main(String[] args) {
+        SwiftCar obj = new SwiftCar();
+        obj.drive();
+        obj.playMusic();
+    }
+}
+abstract class Car {
+    public abstract void drive();
+    public void playMusic() {
+        System.out.println("music playing");
+    }
+}
+ class SwiftCar extends Car {
+    public  void drive() {
+        System.out.println("Start driving");
+    }
     
 }
